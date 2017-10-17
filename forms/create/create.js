62cdb31ccd07c664ac90521f6030a4d4377f01ca
Scott Kennerly <skennerly@hylete.com>
2017-10-17 15:00:02 -0700

    Updating changes per Global-e

---
+++ forms/create/create.js
@@ -0,0 +1,38 @@
+jQuery( document ).ready(function() {
+
+    var custEmail = getQueryString("email");
+    var codeId = getQueryString("codeid");
+
+    if(codeId == null || custEmail == null) {
+        jQuery("#wb-errorMessage").text("Some data we need is missing. Please make sure you filled out the form correctly and try again.").show();
+    } else{
+        var urlBase =  "https://pbhservice.hylete.com/hyletePBHService.asmx/";
+        var requestData = {email: custEmail, codeId: codeId, expiration: '4'};
+
+        jQuery.ajax({
+            url: "/forms/lib/proxy.php",
+            data: {requrl: urlBase + "CreateWinbackCouponCode?" + jQuery.param(requestData)},
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            cache: false,
+            success: function (data) {
+                console.log(data);
+                if (data.errorMessage != "" && data.ErrorMessage != null) {
+                    jQuery("#wb-errorMessage").text(data.ErrorMessage).show();
+                } else {
+                    jQuery.each(data, function() {
+                        jQuery('#wb-code').text(data.CouponCode);
+                    });
+                }
+            }
+        });
+    }
+
+    function getQueryString ( field, url ) {
+        var href = url ? url : window.location.href;
+        var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+        var string = reg.exec(href);
+        return string ? string[1] : null;
+    }
+
+});
---
+++ forms/create/create.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>HYLETE C3 Challenge</title>
+
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="/forms/js/challenge-global.js"></script>
+    <script src="/forms/create/create.js"></script>
+
+</head>
+<body>
+Your code is: <span id="code"></span>
+</body>
