f9a4def0eb21e62cf89f1d37908d49a60e449e3f
Scott Kennerly <skennerly@hylete.com>
2017-11-15 11:28:58 -0800

    Adding some code to skip Global-e orders

--- app/code/local/Directshop/FraudDetection/Model/Payment/Observer.php
+++ app/code/local/Directshop/FraudDetection/Model/Payment/Observer.php
@@ -21,16 +21,18 @@ class Directshop_FraudDetection_Model_Payment_Observer
 		// don't save the order if there's a fatal error
 		if (empty($res['err']) || !in_array($res['err'], Directshop_FraudDetection_Model_Result::$fatalErrors))
 		{
-			
-			Mage::helper('frauddetection')->saveFraudData($res, $order);
-			$order->setFraudDataTemp($res);
-			
-			if (Mage::getStoreConfig('frauddetection/general/holdwhenflagged') && 
-				$res['ourscore'] >= Mage::getStoreConfig('frauddetection/general/threshold') && 
-				$order->canHold())
-			{
-				$order->hold();
-			}
+            $payment_method_code = $payment->getMethodInstance()->getCode();
+			if($payment_method_code != "globale") {
+                Mage::helper('frauddetection')->saveFraudData($res, $order);
+                $order->setFraudDataTemp($res);
+
+                if (Mage::getStoreConfig('frauddetection/general/holdwhenflagged') &&
+                    $res['ourscore'] >= Mage::getStoreConfig('frauddetection/general/threshold') &&
+                    $order->canHold())
+                {
+                    $order->hold();
+                }
+            }
 		}
 	}
 	
