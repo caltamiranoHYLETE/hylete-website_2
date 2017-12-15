af95adf3b8d2ba9040f5a27a0d66042c5d1c3423
Myles Forrest <myles@mediotype.com>
2017-12-15 16:40:24 -0500

    Add mysql setup script that will hold the value that is to be applied to pricing labels based on the customer group

---
+++ app/code/local/Mediotype/HyletePrice/sql/mediotype_hyleteprice_setup/mysql4-upgrade-0.0.1-0.0.2.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * TODO: Myles: Add column to the customer/customer-group table to house the label that will be used
+ */
+
+$table = $installer->getConnection()->newTable($installer->getTable('customer/customer_group'));
