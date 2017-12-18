ba3e002486f3206934eede9533a17fa8463c1cf4
Myles Forrest <myles@mediotype.com>
2017-12-18 14:21:49 -0500

    Add category attribute for "Is Hylete Price Clearance Category" attribute

--- app/code/local/Mediotype/HyletePrice/Helper/Data.php
+++ app/code/local/Mediotype/HyletePrice/Helper/Data.php
@@ -7,17 +7,32 @@
  */
 class Mediotype_HyletePrice_Helper_Data extends Mage_Core_Helper_Abstract
 {
+	private $_menClearanceCategoryId = "65"; // MYLES: TODO: Refactor into adminhtml form somewhere
+	private $_wommenClearanceCategoryId = "76"; // MYLES: TODO: Refactor into adminhtml form somewhere
+
 	/**
+	 * Mediotype_HyletePrice_Helper_Data constructor.
+	 */
+	public function __construct()
+	{
+		$this->_clearanceCategories = array(
+			$this->_menClearanceCategoryId,
+			$this->_wommenClearanceCategoryId
+		);
+	}
+
+	/**
+	 * @param $currentCategory
 	 * @return string
 	 */
-	public function getPriceLabelByCustomerGroup()
+	public function getPriceLabelByCustomerGroup($currentCategory)
 	{
 		$groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
 		$group = Mage::getModel('customer/group')->load($groupId);
 
 		$label = $group->getCustomerGroupHyletePriceLabel();
 
-		if ($label == null) {
+		if ($label == null || array_contains($this->_clearanceCategories, $currentCategory)) {
 			$group = Mage::getModel('customer/group')->load(0);
 			$label = $group->getCustomerGroupHyletePriceLabel();
 		}
---
+++ app/code/local/Mediotype/HyletePrice/sql/mediotype_hyleteprice_setup/mysql4-upgrade-0.0.1-0.0.2.php
@@ -0,0 +1,20 @@
+<?php
+
+$installer = new Mage_Catalog_Model_Resource_Setup();
+$installer->startSetup();
+
+$installer->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'is_hylete_price_clearance', array(
+	'group' => 'General Information',
+	'input' => 'select',
+	'type' => 'yesno',
+	'label' => 'Is this a Hylete Price Clearance category?',
+	'source' => 'eav/entity_attribute_source_boolean',
+	'backend' => '',
+	'default' => 0,
+	'visible' => true,
+	'required' => false,
+	'visible_on_front' => true,
+	'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+));
+
+$installer->endSetup();
