dd97afd55b6125f5c000b2a392eb5e3d993c22c2
Myles Forrest <myles@mediotype.com>
2017-12-21 16:47:41 -0500

    Add a column to customer_group table to facilitate the CMS blocks being able to be changed in adminhtml

---
+++ app/code/local/Mediotype/HyletePrice/data/mediotype_hyleteprice_setup/data-upgrade-0.0.4-0.0.5.php
@@ -0,0 +1,16 @@
+<?php
+
+// MYLES: Below are the none data we need to insert with this script
+
+/*
+customer_group_id	customer_group_code			customer_group_hylete_price_label	hylete_price_cms_block_identifier
+0					NOT LOGGED IN				Hylete								hylete_price_difference_verbiage_default
+1					Everyday Athlete			NULL								hylete_price_difference_verbiage_default
+5					Pro Deal					NULL								hylete_price_difference_verbiage_other
+6					Influencer/Train Team		NULL								hylete_price_difference_verbiage_other
+8					Influencer/Compete Team		NULL								hylete_price_difference_verbiage_other
+9					Influencer/Service League	NULL								hylete_price_difference_verbiage_other
+10					Influencer/Gym Owner		NULL								hylete_price_difference_verbiage_other
+17					NASM						NULL								hylete_price_difference_verbiage_other
+37					HYLETE Investors			Investor							hylete_price_difference_verbiage_investor
+*/
---
+++ app/code/local/Mediotype/HyletePrice/sql/mediotype_hyleteprice_setup/mysql4-upgrade-0.0.3-0.0.4.php
@@ -0,0 +1,15 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+
+$installer
+	->getConnection()
+	->addColumn($installer->getTable('customer/customer_group'),
+		'hylete_price_cms_block_identifier', array(
+			'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+			'length' => 32,
+			'comment' => 'Identifier of the CMS block to be rendered into the tooltip modal',
+		));
+
+$installer->endSetup();
