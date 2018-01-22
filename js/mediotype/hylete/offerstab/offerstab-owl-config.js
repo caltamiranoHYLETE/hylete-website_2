cf5cbe08e0575faa367f7d60c36337678095aecc
Myles Forrest <myles@mediotype.com>
2018-01-22 12:41:43 -0500

    Maneuvering scripts and styles into position jQuery noconflict so carousel is working

---
+++ js/mediotype/hylete/offerstab/offerstab-owl-config.js
@@ -0,0 +1,26 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function ($) {
+    jQuery('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 40,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+
+            600: {
+                items: 2,
+                nav: false
+            },
+
+            1000: {
+                items: 4,
+                nav: true,
+                loop: false
+            }
+        }
+    })
+});
