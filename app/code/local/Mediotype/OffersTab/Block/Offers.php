5e5a2f87b3288ab1d193e91766a53d98bca41eea
Myles Forrest <myles@mediotype.com>
2018-01-22 15:11:19 -0500

    Add helper for module Stub out call to determine which offers to show

--- app/code/local/Mediotype/OffersTab/Block/Offers.php
+++ app/code/local/Mediotype/OffersTab/Block/Offers.php
@@ -2,9 +2,15 @@
 
 /**
  * Class Mediotype_OffersTab_Block_Offers
+ *
+ * @author Myles Forrest <myles@mediotype.com>
  */
 class Mediotype_OffersTab_Block_Offers extends Mage_Core_Block_Template
 {
+	protected $_offersTabHelper;
+
+	protected $_offers; // Will hold array of offers to show user
+
 	/**
 	 * Mediotype_OffersTab_Block_Offers constructor.
 	 * @param array $args
@@ -12,5 +18,10 @@ class Mediotype_OffersTab_Block_Offers extends Mage_Core_Block_Template
 	public function __construct(array $args = array())
 	{
 		parent::__construct($args);
+
+		$this->_offersTabHelper = Mage::helper("mediotype_offerstab");
+
+		// Populate $this->_offers appropriately
+		$this->_offers = $this->_offersTabHelper->getFilteredOffers();
 	}
 }
---
+++ app/code/local/Mediotype/OffersTab/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Class Mediotype_OffersTab_Helper_Data
+ *
+ * @author Myles Forrest <myles@mediotype.com>
+ */
+class Mediotype_OffersTab_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	/**
+	 * Mediotype_HyletePrice_Helper_Data constructor.
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Responsible for returning a list of CMS static block ids to display
+	 */
+	public function getFilteredOffers()
+	{
+		$test = "test";
+	}
+}
