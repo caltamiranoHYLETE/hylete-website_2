03ac64130678688a62689bf7ef16bc06a6ac29a6
Myles Forrest <myles@mediotype.com>
2018-01-22 15:02:15 -0500

    Missed this added file last commit

---
+++ app/code/local/Mediotype/OffersTab/Block/Offers.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class Mediotype_OffersTab_Block_Offers
+ */
+class Mediotype_OffersTab_Block_Offers extends Mage_Core_Block_Template
+{
+	/**
+	 * Mediotype_OffersTab_Block_Offers constructor.
+	 * @param array $args
+	 */
+	public function __construct(array $args = array())
+	{
+		parent::__construct($args);
+	}
+}
