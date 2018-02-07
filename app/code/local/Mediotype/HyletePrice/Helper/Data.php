20f47dc2af0eae1c3c21e3a841c9571dd1150ed0
Matt Sherer <matt@mediotype.com>
2018-02-07 15:36:56 -0600

    HYL-39 - Adding regular price above clearance price on clearance categories.

--- app/code/local/Mediotype/HyletePrice/Helper/Data.php
+++ app/code/local/Mediotype/HyletePrice/Helper/Data.php
@@ -7,80 +7,80 @@
  */
 class Mediotype_HyletePrice_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	/**
-	 * @param $categoryId
-	 * @return mixed|string
-	 */
-	public function isClearanceCategory($categoryId)
-	{
-		$attributeValue = Mage::getModel('catalog/category')->load($categoryId)->getData('is_hylete_price_clearance');
-		if ($attributeValue == null) {
-			$attributeValue = "0";
-		}
-		return $attributeValue;
-	}
+    /**
+     * @param $categoryId
+     * @return mixed|string
+     */
+    public function isClearanceCategory($categoryId)
+    {
+        $attributeValue = Mage::getModel('catalog/category')->load($categoryId)->getData('is_hylete_price_clearance');
+        if ($attributeValue == null) {
+            $attributeValue = "0";
+        }
+        return $attributeValue;
+    }
 
-	/**
-	 * @param $currentCategory
-	 * @return string
-	 */
-	public function getPriceLabelByCustomerGroup($currentCategory = null)
-	{
-		$groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
-		$group = Mage::getModel('customer/group')->load($groupId);
+    /**
+     * @param $currentCategory
+     * @return string
+     */
+    public function getPriceLabelByCustomerGroup($currentCategory = null)
+    {
+        $groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
+        $group = Mage::getModel('customer/group')->load($groupId);
 
-		$label = $group->getCustomerGroupHyletePriceLabel();
+        $label = $group->getCustomerGroupHyletePriceLabel();
 
-		if ($label == null) {
-			$group = Mage::getModel('customer/group')->load(0);
-			$label = $group->getCustomerGroupHyletePriceLabel();
-		}
+        if ($label == null) {
+            $group = Mage::getModel('customer/group')->load(0);
+            $label = $group->getCustomerGroupHyletePriceLabel();
+        }
 
-		if ($currentCategory && $this->isClearanceCategory($currentCategory)) {
-			$label = "Clearance";  // MYLES: No reason not to make this configurable as well
-		}
+        if ($currentCategory && $this->isClearanceCategory($currentCategory)) {
+            $label = "clearance";  // MYLES: No reason not to make this configurable as well
+        }
 
-		$postamble = " price";
+        $postamble = " price";
 
-		return $label . $postamble;
-	}
+        return $label . $postamble;
+    }
 
-	/**
-	 * @param $currentCategory
-	 * @return string
-	 */
-	public function getPriceLabelByCustomerGroupAndProduct($currentCategory, $currentProduct)
-	{
-		$groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
-		$group = Mage::getModel('customer/group')->load($groupId);
+    /**
+     * @param $currentCategory
+     * @return string
+     */
+    public function getPriceLabelByCustomerGroupAndProduct($currentCategory, $currentProduct)
+    {
+        $groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
+        $group = Mage::getModel('customer/group')->load($groupId);
 
-		$label = $group->getCustomerGroupHyletePriceLabel();
+        $label = $group->getCustomerGroupHyletePriceLabel();
 
-		if ($label == null) {
-			$group = Mage::getModel('customer/group')->load(0);
-			$label = $group->getCustomerGroupHyletePriceLabel();
-		}
+        if ($label == null) {
+            $group = Mage::getModel('customer/group')->load(0);
+            $label = $group->getCustomerGroupHyletePriceLabel();
+        }
 
-		if ($this->isClearanceCategory($currentCategory)) {
-			$label = "Clearance"; // MYLES: No reason not to make this configurable as well
-		}
+        if ($this->isClearanceCategory($currentCategory)) {
+            $label = "clearance"; // MYLES: No reason not to make this configurable as well
+        }
 
-		$label .= " price"; // MYLES: This is where the read to a configurable value in adminhtml needs to go
+        $label .= " price"; // MYLES: This is where the read to a configurable value in adminhtml needs to go
 
-		// MYLES: Determine what to do on collisions with group label
-		if($currentProduct->getIsOnFlashSale()) {
-			$label = "<span style=\"color:#34BAF3\">FLASH SALE</span>";
-		}
+        // MYLES: Determine what to do on collisions with group label
+        if($currentProduct->getIsOnFlashSale()) {
+            $label = "<span style=\"color:#34BAF3\">FLASH SALE</span>";
+        }
 
-		return $label;
-	}
+        return $label;
+    }
 
     /**
      * @param Mage_Catalog_Model_Product $currentProduct
      * @param bool $isCategoryDetailsPage
      * @return string
      */
-	public function hasProductMSRP($currentProduct, $isProductDetailsPage = false)
+    public function hasProductMSRP($currentProduct, $isProductDetailsPage = false)
     {
         if (!$currentProduct || !$currentProduct->getMsrp()) {
             return '';
@@ -89,22 +89,22 @@ class Mediotype_HyletePrice_Helper_Data extends Mage_Core_Helper_Abstract
         return 'hylete-price-label-' . ($isProductDetailsPage ? 'lg' : 'sm');
     }
 
-	/**
-	 * @return mixed
-	 */
-	public function getPriceDifferenceCmsBlockByCustomerGroup()
-	{
-		$groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
+    /**
+     * @return mixed
+     */
+    public function getPriceDifferenceCmsBlockByCustomerGroup()
+    {
+        $groupId = $groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
 
-		/** @var Mage_Customer_Model_Group $group */
-		$group = Mage::getModel('customer/group')->load($groupId);
+        /** @var Mage_Customer_Model_Group $group */
+        $group = Mage::getModel('customer/group')->load($groupId);
 
-		$groupCmsBlock = $group->getHyletePriceCmsBlockIdentifier();
+        $groupCmsBlock = $group->getHyletePriceCmsBlockIdentifier();
 
-		if ($groupCmsBlock == null) {
-			$groupCmsBlock = "hylete_price_difference_verbiage_default";
-		}
+        if ($groupCmsBlock == null) {
+            $groupCmsBlock = "hylete_price_difference_verbiage_default";
+        }
 
-		return Mage::app()->getLayout()->createBlock('cms/block')->setBlockId($groupCmsBlock)->toHtml();
-	}
+        return Mage::app()->getLayout()->createBlock('cms/block')->setBlockId($groupCmsBlock)->toHtml();
+    }
 }
