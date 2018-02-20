360cb9ce8253d03bfb2e790bf563dc1aaea738f5
Matt Sherer <matt@mediotype.com>
2018-02-20 14:40:40 -0600

    HYL-52 - Correcting calculations in the checkout cart

--- app/code/local/Mediotype/HyletePrice/Model/Observer.php
+++ app/code/local/Mediotype/HyletePrice/Model/Observer.php
@@ -28,12 +28,19 @@ class Mediotype_HyletePrice_Model_Observer extends Amasty_Rules_Model_Observer
 		return $this;
 	}
 
+    /**
+     * @param Varien_Event_Observer $observer
+     * @return bool
+     */
 	public function calculateProductFinalPriceWithMsrp(Varien_Event_Observer $observer)
     {
+        $quote = Mage::getSingleton('checkout/session');
+        $hyletePriceHelper = Mage::helper('mediotype_hyleteprice');
+        $hasMsrpTargetRule = $hyletePriceHelper->quoteHasMsrpTargetRule($quote);
         $product = $observer->getProduct();
         $msrp = $product->getMsrp();
 
-        if (is_null($msrp) || !$msrp) {
+        if (!$hasMsrpTargetRule || is_null($msrp) || !$msrp) {
             return false;
         }
 
