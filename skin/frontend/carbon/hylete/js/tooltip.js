d2526eefb42d41c5fd88f93e354ba4dd8cf671df
Matt Sherer <matt@mediotype.com>
2018-03-05 10:51:49 -0600

    HYL-51 - Changing mouseenter/mouseleave to click for mobile tooltip bug

--- skin/frontend/carbon/hylete/js/tooltip.js
+++ skin/frontend/carbon/hylete/js/tooltip.js
@@ -14,7 +14,7 @@
         {
             var that = this;
 
-            $(this.options.tooltipLink).on('mouseenter mouseleave', function(e) {
+            $(this.options.tooltipLink).on('click', function(e) {
                 e.preventDefault();
                 $(this)
                     .closest(that.options.tooltipContainer)
@@ -22,29 +22,29 @@
                     .toggleClass(that.options.tooltipActiveClass);
             });
 
-            $(this.options.tooltipLink).on('touchstart', function(e) {
-                e.preventDefault();
-
-                var $currentTooltip = $(this).closest(that.options.tooltipContainer).find(that.options.tooltip);
-                var $currentTooltipLink = $(this);
-                $('.tooltip').not($currentTooltip).removeClass(that.options.tooltipActiveClass);
-                $(that.options.tooltipLink).not($currentTooltipLink).removeClass('is-touched');
-
-                if($(this).hasClass('is-touched')) {
-                    $(this)
-                        .removeClass('is-touched')
-                        .closest(that.options.tooltipContainer)
-                        .find(that.options.tooltip)
-                        .removeClass(that.options.tooltipActiveClass);
-                }
-                else {
-                    $(this)
-                        .addClass('is-touched')
-                        .closest(that.options.tooltipContainer)
-                        .find(that.options.tooltip)
-                        .addClass(that.options.tooltipActiveClass);
-                }
-            });
+            // $(this.options.tooltipLink).on('touchstart', function(e) {
+            //     e.preventDefault();
+            //
+            //     var $currentTooltip = $(this).closest(that.options.tooltipContainer).find(that.options.tooltip);
+            //     var $currentTooltipLink = $(this);
+            //     $('.tooltip').not($currentTooltip).removeClass(that.options.tooltipActiveClass);
+            //     $(that.options.tooltipLink).not($currentTooltipLink).removeClass('is-touched');
+            //
+            //     if($(this).hasClass('is-touched')) {
+            //         $(this)
+            //             .removeClass('is-touched')
+            //             .closest(that.options.tooltipContainer)
+            //             .find(that.options.tooltip)
+            //             .removeClass(that.options.tooltipActiveClass);
+            //     }
+            //     else {
+            //         $(this)
+            //             .addClass('is-touched')
+            //             .closest(that.options.tooltipContainer)
+            //             .find(that.options.tooltip)
+            //             .addClass(that.options.tooltipActiveClass);
+            //     }
+            // });
 
             $(this.options.tooltipClose).on('touchstart', function(e) {
                 e.preventDefault();
