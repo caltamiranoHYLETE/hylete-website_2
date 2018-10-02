6937dfc38553e9fe414e25efde52a72187bcabb6
Daniel Sousa <Daniel Sousa@localhost>
2018-10-02 22:45:30 +0100

    HYLT-148 - Update configurations, cms blocks and remove extra font awesome css files

---
+++ app/code/local/Mediotype/HyleteBase/data/mediotype_hyletebase_setup/data-install-1.0.0.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author    ${userEmail}
+ * @copyright 2018 ${company}. All rights reserved.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+Mage::getConfig()->saveConfig('carbon/settings/icons', 'disable');
+
+
+/** update checkout icon */
+$installer->getConnection()->query(
+    "UPDATE cms_block SET content = REPLACE(
+                    content, 
+                    '<i class=\"fa-li fa fa-exclamation-triangle\"></i>', 
+                    '<i class=\"fas fa-exclamation-triangle\"></i>'
+                    )"
+);
+
+$installer->getConnection()->query(
+    "UPDATE cms_block SET content = REPLACE(
+                    content, 
+                    '<i class=\"fa-li fa fa-calendar\"></i>', 
+                    '<i class=\"fal fa-calendar-alt\"></i>'
+                    )"
+);
+
+$installer->endSetup();
