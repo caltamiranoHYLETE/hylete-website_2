7e403730eb61ee74fc3f7ebf75ea58b306add520
Daniel Sousa <Daniel Sousa@localhost>
2018-10-04 13:51:28 +0100

    HYLT-150 - update static block - blue_bar_satisfaction_guaranteed

---
+++ app/code/local/Mediotype/HyleteBase/data/mediotype_hyletebase_setup/data-upgrade-1.0.0-1.0.1.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author    Mediotype Developement <diveinto@mediotype.com>
+ * @copyright 2018 Mediotype. All rights reserved.
+ */
+
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+/** Update Satisfaction bar */
+$block = Mage::getModel('cms/block')
+    ->getCollection()
+    ->addFieldToFilter('identifier', 'blue_bar_satisfaction_guaranteed')
+    ->getFirstItem();
+if ($block) {
+    $block->setData('content', '
+        <div id="top-announcement" class="owl-carousel owl-theme">
+            <div class="owl-item ">
+                100% fit guarantee + free U.S. return shipping
+            </div>
+        </div>')
+        ->save();
+}
+
+$installer->endSetup();
