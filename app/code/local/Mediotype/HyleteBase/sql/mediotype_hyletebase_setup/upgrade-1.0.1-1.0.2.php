0fc666cd8ed5b5e9b6d4e978c12120e78596f7af
M?rti?? Saukums <martins@saukums.lv>
2018-11-20 15:10:00 +0200

    HYLT-172 - add category image hover + attribute fix

---
+++ app/code/local/Mediotype/HyleteBase/sql/mediotype_hyletebase_setup/upgrade-1.0.1-1.0.2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author    Mediotype Developement <diveinto@mediotype.com>
+ * @copyright 2018 Mediotype. All rights reserved.
+ */
+
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+$installer->updateAttribute('catalog_product', 'rollover_image', 'used_in_product_listing', '1');
+$installer->updateAttribute('catalog_product', 'rollover_image', 'is_visible_on_front', '1');
+
+$installer->endSetup();
