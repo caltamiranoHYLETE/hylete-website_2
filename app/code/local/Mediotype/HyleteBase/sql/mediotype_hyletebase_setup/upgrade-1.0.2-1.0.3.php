3e30c73e7c786d68804d5f4489f6e10c8a45db33
Daniel Sousa <Daniel Sousa@localhost>
2019-01-17 17:17:49 +0000

    HYLT-181 Add a title above the promo/gift card section of cart page

---
+++ app/code/local/Mediotype/HyleteBase/sql/mediotype_hyletebase_setup/upgrade-1.0.2-1.0.3.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author    Mediotype Developement <diveinto@mediotype.com>
+ * @copyright 2018 Mediotype. All rights reserved.
+ */
+
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+
+$identifier = 'cart_promo_title';
+$title = 'Cart promo title';
+$contentCmsBlock = <<<EOT
+<h2 class="promo-title">promo codes & Gift cards</h2>
+EOT;
+$cmsBlock = Mage::getModel('cms/block')->load($identifier);
+$cmsBlock->setTitle($title)
+    ->setContent($contentCmsBlock)
+    ->setIdentifier($identifier)
+    ->setStores(0)
+    ->setIsActive(true);
+$cmsBlock->save();
+$installer->endSetup();
