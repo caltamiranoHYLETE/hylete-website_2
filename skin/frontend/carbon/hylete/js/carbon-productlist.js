50a25f1e5f194983840f2c2e80a10818f45cc827
Daniel Sousa <danielsousa@mediotype.com>
2019-03-08 18:32:59 +0000

    HYLT-213 add ajax event when removing filters and sorting products

--- skin/frontend/carbon/hylete/js/carbon-productlist.js
+++ skin/frontend/carbon/hylete/js/carbon-productlist.js
@@ -3,6 +3,16 @@
 
     if ("ajaxProductList" in $.vaimo) {
         $.widget("vaimo.ajaxProductList", $.vaimo.ajaxProductList, {
+            options: {
+                eventSelectors: {
+                    anchors: {
+                        '.block-layered-nav-wrapper .remove-item-list a': true,
+                        '.toolbar .sort-by a' : true,
+                        '.block-layered-nav a': true,
+                        '.category-products .toolbar a': true
+                    }
+                }
+            },
             _applyResponse: function(transport) {
                 this._super(transport);
 
