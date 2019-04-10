9a43270045c84d9b7d176cafaa7b12b259d04223
daniel <daniel@mediotype>
2019-04-10 17:04:57 +0100

    HYLT-236 size guide, media gallery

--- skin/frontend/carbon/hylete/js/product.js
+++ skin/frontend/carbon/hylete/js/product.js
@@ -38,4 +38,74 @@ jQuery(document).ready(function ($) {
             width: $getPercent + '%'
         }, animationLength);
     }
+
+    /** store resize status */
+    var resizeStatus;
+
+    /**
+     * Refresh MagicScroll plugin
+     *
+     * @return void
+     */
+    function resizeThumbnails(){
+        var elem = $('.product-essential .MagicScroll');
+        if (elem.length) {
+            var data = elem.data('options'),
+                options = getOptions(data),
+                newOrientation = getNewOrientation();
+
+            if (options.orientation !== newOrientation) {
+                var mergedData = $.extend(options, {'orientation': newOrientation});
+                var strings = [];
+                $.each(mergedData, function(index,value) {
+                    var str = index + ":" + value;
+                    strings.push(str);
+                });
+                var result = strings.join(";") + ';';
+                elem.attr('data-options',result);
+                elem.removeData('options');
+                MagicScroll.refresh();
+            }
+        }
+
+    }
+
+    /**
+     * Retrieve new Orientation according to breakpoint
+     *
+     * @return {string}
+     */
+    function getNewOrientation() {
+        // @media only screen and (max-width: 767px) = xs
+        if (window.innerWidth > 767) {
+            return 'vertical';
+        }
+        return 'horizontal';
+    }
+
+    /**
+     *  Retrieve options as object
+     *
+     * @param {string} data
+     * @returns {{}}
+     */
+    function getOptions(data) {
+
+        return data.split(";").reduce(function(obj, str, index) {
+            let strParts = str.split(":");
+            if (strParts[0] && strParts[1]) {
+                obj[strParts[0].replace(/\s+/g, '')] = strParts[1].trim();
+            }
+            return obj;
+        }, {});
+
+    }
+
+    window.onresize = function() {
+        clearTimeout(resizeStatus);
+        resizeStatus = setTimeout(function() {
+            resizeThumbnails();
+        }, 100);
+    };
+
 }); 
