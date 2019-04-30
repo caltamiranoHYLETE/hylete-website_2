7457ede1437e7bb4fb4006ff93a52071df7cb4cd
Damir Korpar <damir@mediotype.com>
2019-04-30 17:06:45 +0200

    HYLT-264 Custom reward point calculation on PDP

--- app/code/local/Hylete/Rewards/Helper/Data.php
+++ app/code/local/Hylete/Rewards/Helper/Data.php
@@ -7,6 +7,7 @@
 class Hylete_Rewards_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const XML_PATH_REWARD_POINTS_TOOLTIP = 'rewards/autointegration/product_view_page_product_points_tooltip';
+    const XML_PATH_REWARD_CURRENCY = 'rewards/autointegration/product_view_page_product_points_currency';
 
     /**
      * Retrieve reward points tooltip content
@@ -24,5 +25,16 @@ class Hylete_Rewards_Helper_Data extends Mage_Core_Helper_Abstract
         return $this->getLayout()->createBlock('cms/block')
             ->setBlockId($blockId)
             ->toHtml();
+    }
+
+    /**
+     * Return rewards currency ID used on product page
+     *
+     * @param null|string|int $store
+     * @return int
+     */
+    public function getProductCurrencyId($store = null)
+    {
+        return (int)Mage::getStoreConfig(self::XML_PATH_REWARD_CURRENCY, $store);
     }
 }
