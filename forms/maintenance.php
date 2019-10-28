da9b77e340cff68e50e95f2a2448e2f25934c1b9
Scott Kennerly <skennerly@hylete.com>
2019-10-28 11:48:36 -0700

    update/updating maintenance mode files to allow for local dev

--- forms/maintenance.php
+++ forms/maintenance.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="https://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>HYLETE is currently undergoing planned maintenance.</title>
     <meta name="robots" content="noindex, nofollow">
@@ -17,44 +17,9 @@
         <div class="col-md-12 column">
             <div class="row clearfix">
                 <div class="col-md-12 column" id="container-second" style="text-align: center;margin-top:40px;">
-                    <div><h4>We're Still Here!</h4><h4>HYLETE has recently moved servers and we're here to help.</h4>
-                        <p>Sometimes when you move servers it takes a while for the internet to catch up. <br/>Most of the time,
-                            it will take under 24 hours for the world's computers to know about the move but if you can't wait,
-                        <br>Here are a few steps that may help you get to the new site:</p></div>
+                    <div><h4>We're Still Here!</h4><h4>HYLETE is undergoing planned maintenance.</h4>
+                        <p>Don't panic. We are updating our site right now and should be back up within 15 minutes. Please, check back soon.</p></div>
                 </div>
-
-                <div class="col-md-12 column" style="margin-top:20px;">
-                    <div class="col-md-4 column" ></div>
-                    <div class="col-md-4 column" >
-                        <h5>The Easy Way:</h5>
-                        <div style="">
-                            <ul>
-                                <li>Windows: Press "Ctrl + Shift + R" </li>
-                                <li>Mac: Press "Command + Shift + R" </li>
-                                <li>iPhone: Turn Airplane Mode on and off</li>
-                                <li>Android type in Chrome:  chrome://net-internals/#dns</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-4 column" ></div>
-                </div>
-
-                <div class="col-md-12 column" style="margin-top:20px;">
-                    <div class="col-md-4 column" ></div>
-                    <div class="col-md-4 column" >
-                        <h5>The Nuclear Option:</h5>
-                        <p>If the easy way didn't work, you can try this:</p>
-                        <div style="">
-                            <ul>
-                                <li><a target="_blank" href="https://documentation.cpanel.net/display/CKB/How+To+Clear+Your+DNS+Cache">How to clear your DNS cache</a></li>
-                            </ul>
-                        </div>
-                        <br><br>
-                        <p>Otherwise, you can wait a bit longer and the move should sort itself out. If you keep getting this screen, <br>email <a href="mailto:customersupport@hylete.com">customersupport@hylete.com</a> and we'll help you out.</p>
-                    </div>
-                    <div class="col-md-4 column" ></div>
-                </div>
-
             </div>
         </div>
     </div>
