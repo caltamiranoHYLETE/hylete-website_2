3b94ffabd3a8821c56b0e6fa3eaa78e2ceef5ced
Matias Delgado <matias@bestworldsweb.com>
2019-10-31 14:19:40 -0300

    fix issue on page load

--- skin/frontend/base/default/bestworlds/klaviyoextend/js/observer.js
+++ skin/frontend/base/default/bestworlds/klaviyoextend/js/observer.js
@@ -1,28 +1,27 @@
 jQuery(function() {
     if (!Mage.Cookies.get('bw_klaviyoextend_off')) {
-        fireKlaviyoCall();
+        var interval = setInterval(fireKlaviyoCall,500);
     }
-});
-
-function fireKlaviyoCall(){
-    if (jQuery('#klaviyoExtendAction').length) {
-        var klaviyoData = _learnq.identify();
-        if (klaviyoData.$email) {
-            jQuery.ajax({
-                url: jQuery('#klaviyoExtendAction').val(),
-                type: "POST",
-                data: {'isAjax':1,'email':klaviyoData.$email},
-                success: function(data) {
-                    console.log('Email from klaviyo was saved!');
-                    Mage.Cookies.set('bw_klaviyoextend_off', true);
-                },
-                error:function(data) {
-                    console.log('Error saving email from Klaviyo');
-                }
-            });
-        } else {
-            Mage.Cookies.set('bw_klaviyoextend_off', true);
+    function fireKlaviyoCall(){
+        if (jQuery('#klaviyoExtendAction').length && jQuery.isFunction(_learnq.identify)) {
+            clearInterval(interval);
+            var klaviyoData = _learnq.identify();
+            if (klaviyoData.$email) {
+                jQuery.ajax({
+                    url: jQuery('#klaviyoExtendAction').val(),
+                    type: "POST",
+                    data: {'isAjax':1,'email':klaviyoData.$email},
+                    success: function(data) {
+                        console.log('Email from klaviyo was saved!');
+                        Mage.Cookies.set('bw_klaviyoextend_off', true);
+                    },
+                    error:function(data) {
+                        console.log('Error saving email from Klaviyo');
+                    }
+                });
+            } else {
+                Mage.Cookies.set('bw_klaviyoextend_off', true);
+            }
         }
     }
-}
-
+});
