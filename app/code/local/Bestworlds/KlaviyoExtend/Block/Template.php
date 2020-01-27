b6a062e7e6f1eea786e4e1ac72b91df924bc5cd2
Carlos Altamirano <caltamirano@hylete.com>
2020-01-27 14:34:18 -0800

    remove bestworlds klaviyoextend module from main branches

--- app/code/local/Bestworlds/KlaviyoExtend/Block/Template.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Bestworlds_KlaviyoExtend_Block_Template extends Mage_Core_Block_Template
-{
-    public function getAction()
-    {
-        return Mage::getBaseUrl().'klaviyoextend/main/registeremail';
-    }
-}
