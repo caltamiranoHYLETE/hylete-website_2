07f9da34c706f302deb51fffc1d32e738fe13415
Carlos Altamirano <caltamirano@hylete.com>
2020-01-27 14:16:38 -0800

    final edits and changes to gov-x verification popup

--- app/code/local/Hylete/ServiceLeague/Helper/Data.php
+++ app/code/local/Hylete/ServiceLeague/Helper/Data.php
@@ -37,22 +37,13 @@ class Hylete_ServiceLeague_Helper_Data extends Mage_Core_Helper_Abstract
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "client_secret=puZ8gMukdNjPnxBQwOEDFOOA3FJeKyePYBAPCCyz4sw%3D&redirect_uri=https%3A%2F%2Fdev.hylete.com%2Fgovx-auth%2Findex%2Fcode&grant_type=authorization_code&code=$code&client_id=c3071849-15db-e911-80e9-14187735bc96",
+            CURLOPT_POSTFIELDS => "client_secret=puZ8gMukdNjPnxBQwOEDFOOA3FJeKyePYBAPCCyz4sw%3D&redirect_uri=http%3A//0d3ac667.ngrok.io/govx-auth/index/code&grant_type=authorization_code&code=$code&client_id=c3071849-15db-e911-80e9-14187735bc96",
             CURLOPT_HTTPHEADER => array(
-                "Accept: */*",
-                "Accept-Encoding: gzip, deflate",
-                "Cache-Control: no-cache",
-                "Connection: keep-alive",
-                "Content-Length: 248",
-                "Content-Type: application/x-www-form-urlencoded",
-                "Cookie: _pxvid=934c4af4-3181-11ea-ba2b-0242ac120007; ai_user=tUEnk|2020-01-07T19:11:58.172Z; _ga=GA1.2.615827829.1578424318; __insp_wid=797306955; __insp_nv=true; __insp_targlpu=aHR0cHM6Ly9hdXRoLmdvdnguY29tL29hdXRoL3Rva2VuP3Jlc3BvbnNlX3R5cGU9Y29kZSZzdGF0ZT0mY2xpZW50X2lkPWMzMDcxODQ5LTE1ZGItZTkxMS04MGU5LTE0MTg3NzM1YmM5NiZzY29wZT0mcmVkaXJlY3RfdXJpPWh0dHAlM0ElMkYlMkZhMDRjMjA2My5uZ3Jvay5pbyUyRmdvdngtYXV0aA%3D%3D; __insp_targlpt=R292WCBWZXJpZmljYXRpb24%3D; __insp_norec_sess=true; __insp_slim=1578424398961",
-                "Host: auth.govx.com",
-                "Postman-Token: e5c8ac26-085f-4db3-9eac-98d44a79d69e,38fdb0de-0f7c-4012-9e2a-87caf1ffb2d3",
-                "User-Agent: PostmanRuntime/7.20.1",
-                "cache-control: no-cache"
+                "Content-Type: application/x-www-form-urlencoded"
             ),
         ));
 
@@ -117,7 +108,6 @@ class Hylete_ServiceLeague_Helper_Data extends Mage_Core_Helper_Abstract
             Mage::log("sendresponse to helper", null, 'govx-auth.log');
             $this->saveResponse($response);
             return $response;
-
         }
     }
     public function checkIfCustomerExist($customerData){
@@ -137,5 +127,6 @@ class Hylete_ServiceLeague_Helper_Data extends Mage_Core_Helper_Abstract
         return false;
 
     }
+
 }
 	 
