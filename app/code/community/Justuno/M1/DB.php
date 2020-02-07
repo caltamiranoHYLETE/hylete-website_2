a87ac6f288f193c079c0ac74d101603cbaaaeb70
Carlos Altamirano <caltamirano@hylete.com>
2020-02-07 15:24:36 -0800

    justuno update

---
+++ app/code/community/Justuno/M1/DB.php
@@ -0,0 +1,41 @@
+<?php
+use Mage_Core_Model_Resource as RC;
+use Varien_Db_Adapter_Pdo_Mysql as MySQL;
+use Varien_Db_Select as Select;
+// 2019-11-07
+final class Justuno_M1_DB {
+	/**
+	 * 2019-11-07
+	 * @used-by select()
+	 * @used-by Justuno_M1_Orders::stat()
+	 * @used-by app/code/community/Justuno/M1/sql/Justuno_M1/mysql4-install-1.4.3.php
+	 * @return MySQL
+	 */
+	static function conn() {return self::res()->getConnection('write');}
+
+	/**
+	 * 2019-11-07
+	 * @used-by Justuno_M1_Orders::stat()
+	 * @return Select
+	 */
+	static function select() {return self::conn()->select();}
+
+	/**
+	 * 2019-11-07
+	 * @used-by Justuno_M1_Orders::stat()
+	 * @used-by app/code/community/Justuno/M1/sql/Justuno_M1/mysql4-install-1.4.3.php
+	 * @param string $s
+	 * @return string
+	 */
+	static function t($s) {return self::res()->getTableName($s);}
+
+	/**
+	 * 2019-11-07
+	 * @used-by conn()
+	 * @used-by t()
+	 * @return RC
+	 */
+	private static function res() {static $r; return $r ?: new RC;}
+}
+
+
